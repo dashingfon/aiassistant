2ce28323b20aeea70db6ae2527109823c56645aa
Mfon Nwa <mfonnwa@gmail.com>
2024-03-18 08:41:34 +0100

    finally got a response from a llm, ready to add the retrieval part

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,35 +1,52 @@
 import os
+import pathlib
 from dotenv import load_dotenv,find_dotenv
 from langchain.schema import (
     AIMessage,
     HumanMessage,
     SystemMessage
 )
+import pinecone
+from operator import itemgetter
 from langchain_openai import ChatOpenAI
-# from langchain_community import OpenAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.chat_models import ChatCohere
 from langchain_community.vectorstores import Pinecone
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain, create_history_aware_retriever
 
 
 load_dotenv(find_dotenv())
+output_parser = StrOutputParser()
+embeddings = OpenAIEmbeddings()
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.3)
-messages = [
-    SystemMessage(content="You are an expert data summarizer"),
-    HumanMessage(content="hi"),
-]
-result = llm.invoke(messages)
+# pinecone.init(
+#     api_key=os.getenv('PINECONE_API_KEY'),  
+#     environment=os.getenv('PINECONE_ENV')  
+# )
+
+# llm = ChatOpenAI()
+llm = ChatCohere()
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are an expert data summarizer"),
+    ("user", "what is the size of the earth {input}")
+])
+chain = prompt | llm | output_parser
+
+result = chain.invoke({"input": "Mr"})
 print(result)
 
-# response=llm(messages)
 
-# print(response.content, end='\n')
-
-
-def chat_response():
+def save_document() -> None:
     ...
+
+
+def chat_response(prompt: str) -> str:
+    return ""
 
 
 if __name__ == "__main__":
